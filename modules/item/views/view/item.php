14acbb3f88f61698ebba88b21daa54ecf097a17a
Simon Nouwens <simonnouwens@gmail.com>
2015-08-17 09:05:33 +0200

    UI fixes for mobile menubar and user settings

--- modules/item/views/view/item.php
+++ modules/item/views/view/item.php
@@ -1,5 +1,45 @@
 <?php
-echo $this->render('../../../search/views/search/item', [
-    'model' => $model
-]);
+// todo make this a configurable item widget
+use yii\helpers\Url;
+use app\components\WidgetRequest;
 ?>
+
+<div class="item card-width col-xs-12 col-sm-6 col-md-4">
+    <a href="<?= Url::to('@web/item/'.$model->id) ?>">
+        <div class="card">
+            <div class="image"
+                 style="<?= ImageHelper::bgImg($model->getImageName(0), ['q' => 90, 'w' => 600]) ?>; background-size: cover; background-position: 50% 50%;">
+                <div class="price-badge"><span>dkk</span> <?= $model->price_week ?></div>
+            </div>
+            <div class="content">
+                <h4 class="category">
+                    <?php
+                    $categories = [];
+                    foreach ($model->categories as $c) {
+                        if($c->type !== 'main') continue;
+                        if(count($categories) >= 2) continue;
+                        $categories[] = $c->name;
+                    }
+                    echo implode($categories, ' ,');
+                    ?>
+                </h4>
+
+                <h3 class="title" style="height:20px;">
+                    <?= $model->name ?>
+                </h3>
+
+                <div class="footer">
+                    <div class="author">
+                        <?= WidgetRequest::request(WidgetRequest::USER_PROFILE_IMAGE, [
+                            'user_id' => $model->owner_id,
+                            'width' => '30px'
+                        ])?>
+                    </div>
+                    <div class="stats pull-right">
+                        <i class="fa fa-map-marker"></i><?= (int)$model->distance ?> km
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+</div>
