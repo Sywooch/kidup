554cca287600c2a1366e88b710d0bb36dcb3a67d
Simon Nouwens <simonnouwens@gmail.com>
2015-08-26 22:29:53 +0200

    Updated circle-CI

--- commands/DeployController.php
+++ commands/DeployController.php
@@ -8,9 +8,14 @@ use yii\console\Controller;
 class DeployController extends Controller
 {
 
-    public function afterDeploy($buildNum){
+    public function afterDeploy($buildNum = null){
         // clear all cache
-
-        //
+        \Yii::$app->cache->flush();
+        // new buildnumber
+        $fileName = Yii::$aliases['@runtime'].'/buildnum';
+        if(!is_file($fileName)){
+            $buildNum = date("YY.MM").'.1';
+        }
+//        file_get_contents(Yii::$aliases['@runtime'].'/buildnum');
     }
 }
