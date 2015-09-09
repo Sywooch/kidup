0275dcaeaf968ec5f32f14a5a98891f57dbd96ea
Simon Nouwens <simonnouwens@gmail.com>
2015-09-10 00:46:54 +0200

    Finilazing feature

--- modules/item/views/view/item.php
+++ modules/item/views/view/item.php
@@ -12,7 +12,7 @@ use yii\helpers\Url;
             <div class="image"
                  style="<?= ImageHelper::bgImg($model->getImageName(0),
                      ['q' => 90, 'w' => 600]) ?>; background-size: cover; background-position: 50% 50%;">
-                <div class="price-badge"><span>dkk</span> <?= $model->price_week ?></div>
+                <div class="price-badge"><span>kr.</span> <?= $model->price_week ?> <?= Yii::t("item", "/ week") ?></div>
             </div>
             <div class="content">
                 <h4 class="category">
@@ -36,12 +36,6 @@ use yii\helpers\Url;
                 </h3>
 
                 <div class="footer">
-                    <div class="author">
-                        <?= WidgetRequest::request(WidgetRequest::USER_PROFILE_IMAGE, [
-                            'user_id' => $model->owner_id,
-                            'width' => '30px'
-                        ]) ?>
-                    </div>
                     <div class="stats pull-right">
                         <i class="fa fa-map-marker"></i><?= (int)$model->distance ?> km
                     </div>
