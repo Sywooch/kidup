16ca7b93aa639b32fc25b8b07e7dd6477552282a
Philip Jensen <philip@juicylight.com>
2015-10-04 15:15:29 +0200

    Added text back to the front-page image. Added new header to search.php

--- modules/home/views/home/search.php
+++ modules/home/views/home/search.php
@@ -31,11 +31,11 @@ $this->registerJsVariables([
                     ]);
                     ?>
 
-                    <div class="row">
+
                         <div class="col-sm-12 text-center">
-                            <h2><?= Yii::t("home.grid.header", "Find Kids stuff") ?></h2>
+                            <h2><?= Yii::t("home.search.header", "Rent this now") ?></h2>
                         </div>
-                    </div>
+
                     
                     <div class="col-sm-9 col-md-6">
                         <?= $form->field($model, 'query')->widget(Typeahead::className(), [
