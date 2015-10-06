f21375e192cdf9b4649a4cdeb59f4af68c9df3f6
Simon Nouwens <simonnouwens@gmail.com>
2015-10-06 14:21:11 +0200

    Booking view

--- modules/item/views/view/booking_widget.php
+++ modules/item/views/view/booking_widget.php
@@ -34,12 +34,15 @@ $this->registerJsVariables([
     <div class="row price-bg">
         <div class="col-md-12">
             <h4>
-                    <span class="pull-left">
-                        <?= $model->currency->forex_name ?> <?= $model->item->price_day !== null ? $model->item->price_day : round($model->item->price_week / 7) ?> <small><?= Yii::t("item.view.booking_widget.per_day", "/ day") ?></small>
-                        <?= $model->item->price_week ?> <small><?= Yii::t("item.view.booking_widget.per_week", "/ week") ?></small>
-                        <?= $model->item->price_month !== null ? $model->item->price_month . "<small> ".Yii::t("item.view.booking_widget.per_month", "/ month")."</small>" : '' ?>
-                    </span>
+                <?= $model->currency->forex_name ?> <?= $model->item->price_day !== null ? $model->item->price_day : round($model->item->price_week / 7) ?>
+                / <?= $model->item->price_week ?><?= $model->item->price_month !== null ? " / " . $model->item->price_month : '' ?>
             </h4>
+            <div class="times">
+                <?= Yii::t("item.view.booking_widget.day", "day") ?> /
+                <?= Yii::t("item.view.booking_widget.week", "week") ?>
+                <?= $model->item->price_month !== null ? " / " . Yii::t("item.view.booking_widget.month",
+                        "month") : '' ?>
+            </div>
         </div>
     </div>
 
