f8fe663d40d3c8d38ba54e26ba01f0defc88d961
Kevin Jacobs <kevin91nl@gmail.com>
2015-10-15 15:14:36 +0200

    Parameter tuning.

--- modules/search/controllers/SearchController.php
+++ modules/search/controllers/SearchController.php
@@ -64,7 +64,7 @@ class SearchController extends Controller
         $dataProvider = new ActiveDataProvider([
             'query' => $model->getQuery(),
             'pagination' => [
-                'pageSize' => 30,
+                'pageSize' => 20,
             ],
         ]);
 
