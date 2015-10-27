edd6574c3cc6000872228442354d9ad9ad7e9cb7
Simon Nouwens <simonnouwens@gmail.com>
2015-10-27 23:42:07 +0100

    Api: conversation and messages

--- modules/api/controllers/ConversationController.php
+++ modules/api/controllers/ConversationController.php
@@ -2,6 +2,7 @@
 namespace api\controllers;
 
 use api\models\Conversation;
+use yii\data\ActiveDataProvider;
 
 class ConversationController extends Controller
 {
@@ -13,7 +14,7 @@ class ConversationController extends Controller
     public function accessControl(){
         return [
             'guest' => [''],
-            'user' => ['index', 'view', 'create', 'update']
+            'user' => ['index', 'view']
         ];
     }
 
@@ -21,6 +22,17 @@ class ConversationController extends Controller
         $actions = parent::actions();
         unset($actions['delete']);
         unset($actions['update']);
+        unset($actions['create']);
+        unset($actions['index']);
         return $actions;
+    }
+
+    public function actionIndex(){
+        return new ActiveDataProvider([
+            'query' => Conversation::find()->where(['target_user_id' => \Yii::$app->user->id])
+                ->orWhere(['initiater_user_id' => \Yii::$app->user->id])
+                ->innerJoinWith('lastMessage')
+                ->orderBy('message.created_at DESC')
+        ]);
     }
 }
--- modules/api/controllers/MessageController.php
+++ modules/api/controllers/MessageController.php
@@ -1,7 +1,10 @@
 <?php
 namespace api\controllers;
 
+use api\models\Conversation;
 use api\models\Message;
+use yii\data\ActiveDataProvider;
+use yii\web\ServerErrorHttpException;
 
 class MessageController extends Controller
 {
@@ -12,16 +15,47 @@ class MessageController extends Controller
 
     public function accessControl(){
         return [
-            'guest' => ['index', 'view'],
-            'user' => []
+            'guest' => [],
+            'user' => ['create', 'view']
         ];
     }
 
     public function actions(){
         $actions = parent::actions();
         unset($actions['delete']);
-        unset($actions['create']);
+        unset($actions['view']);
+        unset($actions['index']);
         unset($actions['update']);
         return $actions;
+    }
+
+    public function actionView($id){
+        // todo: security check
+        return new ActiveDataProvider([
+            'query' => Message::find()
+                ->where(['conversation_id' => $id])
+                ->orderBy('created_at DESC')
+        ]);
+    }
+
+    public function actionCreate(){
+        // todo: security check
+        $params = \Yii::$app->request->post();
+        /**
+         * @var Conversation $c
+         */
+        $c = Conversation::findOne(['id' => $params['conversation_id']]);
+        $m = new Message();
+        $m->receiver_user_id = $c->otherUser->user_id;
+        $m->read_by_receiver = 0;
+        $m->sender_user_id = \Yii::$app->user->id;
+        $m->message = $params['message'];
+        $m->conversation_id = $c->id;
+        if($m->save()){
+            return $m;
+        }
+
+        throw new ServerErrorHttpException("Could not be saved");
+
     }
 }
