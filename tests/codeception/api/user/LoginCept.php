f2df0b44f10b670156d2886afd5e213c8839ba48
Simon Nouwens <simonnouwens@gmail.com>
2015-11-04 13:55:28 +0100

    Me api helper

--- tests/codeception/api/user/LoginCept.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$I = new ApiTester($scenario);
-$I->wantTo('create a user via API');
-$I->sendGET('items');
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
-$I->seeResponseContainsJson(['pageCount' => 1]);
