a8599235fde7758bc018412f3ddbe870c0fb93b8
Simon Nouwens <simonnouwens@gmail.com>
2015-11-20 15:20:46 +0100

    Solved Payout

--- modules/booking/models/Payout.php
+++ modules/booking/models/Payout.php
@@ -81,16 +81,17 @@ class Payout extends \booking\models\base\Payout
             // todo: what here?
             return false;
         }
+        // 4 number bank identifier + bank account number (no spaces in between)
+        $field[3] = Encrypter::decrypt($payoutMethod->identifier_2_encrypted, $key).Encrypter::decrypt($payoutMethod->identifier_1_encrypted,$key);
         $field[4] = str_replace(".", ",", $this->amount); // to account
         $field[6] = "DKK";
         $field[7] = "N"; // todo check
         $field[13] = "J"; // todo check
 
-        $field[24] = "KidUp-Payout-" . (string)$this->id;
-        $field[83] = "Tak fordi du brugte Kidup!";
-        // to account, this needs to be processed by externall process.php (in veracrypt container)
-        $field[84] = Encrypter::decrypt($payoutMethod->identifier_1_encrypted,$key);
-        $field[85] = Encrypter::decrypt($payoutMethod->identifier_2_encrypted, $key); // to account
+        $field[24] = \Yii::t('payout.payout_identification', 'KidUp Payout {payoutId}', [
+            'payoutId' => (string)$this->id
+        ]);
+        $field[83] = \Yii::t('payout.bank_message_receiver', 'Thanks for using KidUp');
         $str = [];
         for ($i = 1; $i <= 86; $i++) {
             if (!isset($field[$i])) {
