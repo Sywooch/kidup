da8f82417f872081f573f195e5947da35064a5e7
Kevin Jacobs <kevin91nl@gmail.com>
2016-02-24 10:06:46 +0100

    Smaller columns.

--- migrations/m160118_104139_pushNotifications.php
+++ migrations/m160118_104139_pushNotifications.php
@@ -10,13 +10,13 @@ class m160118_104139_pushNotifications extends Migration
         $this->createTable('mobile_devices', [
             'id' => 'INT(11) NOT NULL AUTO_INCREMENT',
             0 => 'PRIMARY KEY (`id`)',
-            'device_id' => 'VARCHAR(500)',
-            'token' => 'VARCHAR(500) UNIQUE',
+            'device_id' => 'VARCHAR(200)',
+            'token' => 'VARCHAR(200) UNIQUE',
             'meta_information' => 'TEXT',
             'user_id' => 'INT(11)',
             'platform' => 'TEXT',
             'is_subscribed' => 'BOOL',
-            'endpoint_arn' => 'VARCHAR(500)',
+            'endpoint_arn' => 'VARCHAR(200)',
             'last_activity_at' => 'INT(11) NOT NULL',
             'created_at' => 'INT(11) NOT NULL'
         ]);
