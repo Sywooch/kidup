7722442b33daffc1c36ea0d0d8092c89801ecc0d
Simon Nouwens <simonnouwens@gmail.com>
2016-02-25 10:31:01 +0100

    Solving #439

---
+++ modules/admin/controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace admin\controllers;
+
+use admin\controllers\Controller;
+use app\helpers\Event;
+use user\models\User;
+
+class TestController extends Controller
+{
+
+    public function actionMail(){
+        $user = User::findOne(['email' => 'simon@tuple.nl']);
+        Event::trigger($user, User::EVENT_USER_REGISTER_DONE);
+    }
+}
