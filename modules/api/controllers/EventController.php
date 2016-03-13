12f0b9dbb5983ef27bc66ead7fa6fa3ed72d7bc2
simon <simonnouwens@gmail.com>
2016-03-13 20:09:25 +0100

    Fixing event api

--- modules/api/controllers/EventController.php
+++ modules/api/controllers/EventController.php
@@ -11,18 +11,11 @@ class EventController extends Controller
     public function actionIndex($type, $data = null, $t = null, $s = null, $l = null, $m = null, $uuid = null)
     {
         $detect = new \Mobile_Detect();
-        // not sure why this is here
-        if (\Yii::$app->session->gCProbability) {
-            if ($detect->is("Bot") || $detect->is("MobileBot")) {
-                // don't track bots
-                exit();
-            }
-        }
         $country = null;
         $city = null;
         $location = LocationFactory::createByIp(\Yii::$app->request->getUserIP());
         if ($location) {
-            $country = $location->country;
+            $country = $location->country0->name;
             $city = $location->city;
         }
         TrackingEvent::track($type, $data, $country, $city, $m, $t, $l, $s, \Yii::$app->request->getUserIP(), $uuid);
